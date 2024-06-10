5e1ddaceb0e06a5a1c6270369f240500784596d3
KjBsty <88504262+KjBsty@users.noreply.github.com>
2024-06-10 22:09:42 +0530

    Update scraper.py
    bug fixes, added story and user info

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,5 +1,6 @@
 import json
-import requests, time
+import requests
+from datetime import datetime, timezone
 from typing import Dict, Optional
 import jmespath
 import logger as log
@@ -117,43 +118,50 @@ def scrape_post_info(url_or_shortcode: str) -> dict:
 
 
 def scrape_user_posts(user_id: str, page_size=50, max_pages: Optional[int] = None):
-    """Scrape all posts of an instagram user of given numeric user id"""
-    base_url = "https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables="
-    variables = {
-        "id": user_id,
-        "first": page_size,
-        "after": None,
-    }
-    _page_number = 1
-    all_posts = []  
-    while True:
-        url = base_url + json.dumps(variables)
-        result = requests.get(url, headers={"x-ig-app-id": INSTAGRAM_APP_ID})
-        data = json.loads(result.content)
-        if "message" in data and data["message"] == 'Please wait a few minutes before you try again.':
-            log.error(f"Rate limit exceeded. Waiting for a minute... | Data response: {data}")
-            time.sleep(60) 
-            continue
-        posts = data["data"]["user"]["edge_owner_to_timeline_media"]
-        for post in posts["edges"]:
-            all_posts.append(parse_post(post["node"]))  
-        page_info = posts["page_info"]
-        if _page_number == 1:
-            # log.success(f"\nscraping total {posts['count']} posts of {user_id}")
-            print(f"\nscraping total {posts['count']} posts of {user_id}")
-        else:
-            # log.success(f"\nscraping posts page {_page_number}\n")
-            print(f"\nscraping posts page {_page_number}\n")
-        if not page_info["has_next_page"]:
-            break
-        if variables["after"] == page_info["end_cursor"]:
-            break
-        variables["after"] = page_info["end_cursor"]
-        _page_number += 1
-        if max_pages and _page_number > max_pages:
-            break
-    
-    return all_posts
+    if user_id != "An error occurred":
+
+        """Scrape all posts of an instagram user of given numeric user id"""
+        base_url = "https://www.instagram.com/graphql/query/?query_hash=e769aa130647d2354c40ea6a439bfc08&variables="
+        variables = {
+            "id": user_id,
+            "first": page_size,
+            "after": None,
+        }
+        _page_number = 1
+        all_posts = []
+        post_count = 0
+        while True:
+            url = base_url + json.dumps(variables)
+            result = requests.get(url, headers={"x-ig-app-id": INSTAGRAM_APP_ID})
+            data = json.loads(result.content)
+            if "message" in data and data["message"] == 'Please wait a few minutes before you try again.':
+                # log.error(f"Rate limit exceeded. Waiting for a minute... | Data response: {data}")
+                print(data["message"])
+                time.sleep(60) 
+                continue
+            posts = data["data"]["user"]["edge_owner_to_timeline_media"]
+            for post in posts["edges"]:
+                all_posts.append(parse_post(post["node"]))  
+            page_info = posts["page_info"]
+            if _page_number == 1:
+                # log.success(f"\nscraping total {posts['count']} posts of {user_id}")
+                print(f"\nscraping total {posts['count']} posts of {user_id}")
+                post_count = posts['count']
+            else:
+                # log.success(f"\nscraping posts page {_page_number}\n")
+                print(f"\nscraping posts page {_page_number}\n")
+            if not page_info["has_next_page"]:
+                break
+            if variables["after"] == page_info["end_cursor"]:
+                break
+            variables["after"] = page_info["end_cursor"]
+            _page_number += 1
+            if max_pages and _page_number > max_pages:
+                break
+        
+        return all_posts, post_count
+    else:
+        return "error"
 
 def scrape_user_id(username):
     url = f"https://i.instagram.com/api/v1/users/web_profile_info/?username={username}"
@@ -166,12 +174,18 @@ def scrape_user_id(username):
     headers = {"x-ig-app-id": INSTAGRAM_APP_ID}
     response = requests.get(url, headers=headers, params=BASE_CONFIG)
 
+    
     if response.status_code == 200:
-        user_data = response.json()
-        user_id = user_data.get('data', {}).get('user', {}).get('id')
-        return(user_id)
+        try:
+            user_data = response.json()
+            user_id = user_data.get('data', {}).get('user', {}).get('id')
+            return(user_id)
+        except:
+            print("An error occurred")
+            return("An error occurred")
+        
     else:
-        return(response.json())
+        return("An error occurred")
     
 
 def user_info(username):
@@ -186,14 +200,44 @@ def user_info(username):
     response = requests.get(url, headers=headers, params=BASE_CONFIG)
 
     if response.status_code == 200:
-        user_data = response.json()
-        with open(f'{username}.json', 'w', encoding='utf-8') as f:
-            json.dump(user_data, f, ensure_ascii=False, indent=4)
-        # return(user_data)
+        try:
+            user_data = response.json()
+            return(user_data)
+        except:
+            return "error"
+        
     else:
-        return(response.json())
+        return "error"
     
 
+def converted_date(timestamp):
+    date = datetime.fromtimestamp(timestamp, timezone.utc)
+    return date.strftime("%d/%m/%Y %H:%M:%S")
+
+def story(username: str):
+    if username != "An error occurred":
+        stories = []
+        try:
+            url = f"https://anonyig.com/api/ig/story?url=https%3A%2F%2Fwww.instagram.com%2Fstories%2F{username}%2F"
+            result = requests.get(url)
+            if result:
+                data = json.loads(result.content)
+                posts = data["result"]
+                for post in posts:
+                    story = {
+                        "img_url" : post["image_versions2"]["candidates"][0]["url"],
+                        "video_url" : post["video_versions"][0]["url"],
+                        "taken_at" : converted_date(post["taken_at"]),
+                        "expires" : converted_date(post["video_versions"][0]["url_signature"]["expires"])
+                    }
+                    stories.append(story)
+                return stories
+            else:
+                return "error"
+        except:
+            return "error"
+    else:
+        return "error"
 
 
 def user_posts(user_id: str, page_size=50, max_pages: Optional[int] = None):
@@ -237,17 +281,3 @@ def user_posts(user_id: str, page_size=50, max_pages: Optional[int] = None):
     #         break
     
     # return all_posts
-
-
-def download_reels(video_url, shortcode):
-    log.info(f"Downloading reels from {video_url} as {shortcode}.mp4")
-    headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36'
-    }
-    try:
-        resp = requests.get(video_url, headers=headers).content
-        with open(f'{shortcode}.mp4', mode='wb') as f:
-            f.write(resp)
-        log.info(f"Reels downloaded successfully as {shortcode}.mp4")
-    except requests.RequestException as e:
-        log.error(f"Failed to download reels from {video_url}: {e}")
